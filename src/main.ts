e465f3a904e1ee4a34712befe54a9162eac87a0e
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-03 21:45:31 +0900

    port change

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// const CLOUDPORT = process.env.PORT || 4000;
-const LOCALPORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(LOCALPORT);
+  await app.listen(PORT);
 }
 bootstrap();
