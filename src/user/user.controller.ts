fa2806a98c000d6129c1fac0117e660da30859e3
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-04 14:59:35 +0900

    userService ange

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -64,7 +64,7 @@ export class UserController {
     @Headers('cookie') cookie: string,
     @Res() res,
   ): Promise<any> {
-    res.this.userService.getEmailAndPhotoByCookie(cookie);
+    res(this.userService.getEmailAndPhotoByCookie(cookie));
   }
 
   @Get('logout')
