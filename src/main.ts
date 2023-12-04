870bcc3d9be0d803a1cc25eecb74070fe09d1efd
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-04 16:02:52 +0900

    Cors Error fixing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({
     origin: 'https://mind-lab-fe-55b3987890a9.herokuapp.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
