75d01f8131be2695f5e263c7f14c6fc03664cf23
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-05 15:31:33 +0900

    Cors Error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_BASEURL,
+    origin: 'https://mind-lab-fe-55b3987890a9.herokuapp.com/',
     credentials: true,
   });
   await app.listen(PORT);
