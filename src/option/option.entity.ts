c58accf3136b1487f0c7231e6094276e009a3774
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-06 21:05:36 +0900

    entity change

--- src/option/option.entity.ts
+++ src/option/option.entity.ts
@@ -1,17 +1,25 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Answer } from 'src/answer/answer.entity';
 import { Question } from 'src/question/question.entity';
+import { Survey } from 'src/survey/survey.entity';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
   OneToMany,
+  AfterInsert,
+  getRepository,
 } from 'typeorm';
 
 @Entity()
 @ObjectType()
 export class Option {
+  async save() {
+    const optionRepository = getRepository(Option);
+    await optionRepository.save(this);
+  }
+
   @Field(() => ID)
   @PrimaryGeneratedColumn('uuid')
   o_id: string;
@@ -24,6 +32,10 @@ export class Option {
   @Column()
   score: number;
 
+  @Field(() => Survey)
+  @ManyToOne(() => Survey, (survey) => survey.questions)
+  survey: Survey;
+
   @Field(() => Question)
   @ManyToOne(() => Question, (question) => question.options)
   question: Question;
@@ -31,4 +43,14 @@ export class Option {
   @Field(() => Answer)
   @OneToMany(() => Answer, (answer) => answer.user)
   answers: Answer[];
+
+  @AfterInsert()
+  async createDefaultOption() {
+    const defaultOption = new Option();
+    defaultOption.text = this.text;
+    defaultOption.score = this.score;
+    defaultOption.survey = this.survey;
+    defaultOption.question = this.question;
+    await defaultOption.save();
+  }
 }
