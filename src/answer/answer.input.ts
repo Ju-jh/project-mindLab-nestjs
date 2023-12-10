49339419ee7ba35b09f8812674968260abd98df8
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-11 04:13:38 +0900

    test

--- src/answer/answer.input.ts
+++ src/answer/answer.input.ts
@@ -3,12 +3,6 @@ import { InputType, Field } from '@nestjs/graphql';
 @InputType()
 export class AnswerInput {
   @Field()
-  userId: string;
-
-  @Field()
-  surveyId: string;
-
-  @Field()
   questionId: string;
 
   @Field()
